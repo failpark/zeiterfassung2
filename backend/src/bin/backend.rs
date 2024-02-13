bf93502c657e31df26ca05ae67e82f51bb667915
Failpark <daniel.matthes@noxa.de>
2024-02-13 11:47:58 +0100

    Chore: ran formatter

--- backend/src/bin/backend.rs
+++ backend/src/bin/backend.rs
@@ -1,6 +1,10 @@
 #[rocket::main]
 async fn main() -> std::result::Result<(), rocket::Error> {
 	let _guard = zeiterfassung_backend::tracing::init();
-	let _rocket = zeiterfassung_backend::rocket().ignite().await?.launch().await?;
+	let _rocket = zeiterfassung_backend::rocket()
+		.ignite()
+		.await?
+		.launch()
+		.await?;
 	Ok(())
 }
